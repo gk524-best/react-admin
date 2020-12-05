a679e1da3698d5126bda2eb54ed73aac43175b09
zhangbingbing <490507385@qq.com>
2020-12-05 20:59:52 +0800

    项目配置

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          edge: '17',
+          firefox: '60',
+          chrome: '67',
+          safari: '11.1',
+        },
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        development: process.env.BABEL_ENV === 'development',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        corejs: 3,
+      },
+    ],
+    '@babel/plugin-proposal-class-properties',
+  ],
+};
