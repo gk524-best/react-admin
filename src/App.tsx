56a57222bc7b5930af00deb425faff78da80d289
zhangbingbing <zhangbingbing@zhangbingbingdeMac-mini.local>
2020-12-22 01:02:14 +0800

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Routes from 'routes/index';
+// import 'fontsource-roboto';
 
 function App() {
   return (
---
+++ src/theme/index.ts
@@ -0,0 +1,7 @@
+import { createMuiTheme, Theme } from "@material-ui/core/styles";
+
+const createTheme = (name: string): Theme => {
+
+}
+
+export default createTheme;
