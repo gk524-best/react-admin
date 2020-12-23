01a26aef389b1e6e4f118f521c8a9befa15e780d
zhangbingbing <zhangbingbing@zhangbingbingdeMac-mini.local>
2020-12-24 00:38:36 +0800

    material主题

---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,17 @@
+import { Breakpoints, Breakpoint } from '@material-ui/core/styles/createBreakpoints'
+
+const breakpoints: Breakpoints = {
+  keys: ['xs', 'sm', 'md', 'lg', 'xl'],
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 960,
+    lg: 1280,
+    xl: 1440
+  },
+  up: (key: Breakpoint | number) => '',
+  down: (key: Breakpoint | number) => '',
+  between: (start: Breakpoint | number, end: Breakpoint | number) => '',
+  only: (key: Breakpoint) => '',
+  width: (key: Breakpoint) => 0
+}
